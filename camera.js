af3f0ca5eda9a82e19449c90a3f103b36991b028
cdewbery <chris.dewbery@gmail.com>
2014-03-22 22:33:05 +1300

    Add basic camera controls
    The camera can be controlled using the w,a,s,d keys and the mouse. Still
    very buggy.

--- camera.js
+++ camera.js
@@ -66,6 +66,11 @@ var Camera = Object.create(Object, {
 	    value: true
 	},
 	
+	_pressed: {
+	    writable: true,
+		value : null,
+	},
+	
 	_view: {
 	    writable : true,
 	    value: null
@@ -76,9 +81,9 @@ var Camera = Object.create(Object, {
 		    if (this._changed) {
 			    var mv = this._view;
 				mat4.identity(mv);
-				mat4.rotateX(mv, this.angles[0]);
-				mat4.rotateZ(mv, this.angles[1]);
-				mat4.rotateY(mv, this.angles[2]);
+				mat4.rotateX(mv, this.angles[0] - Math.PI / 2.0);
+				mat4.rotateZ(mv, this.angles[2]);
+				mat4.rotateY(mv, this.angles[1]);
 				/* simulate camera position at x,y,z by moving to -x,-y,-z */
 				mat4.translate(mv, [-this.position[0], -this.position[1], -this.position[2]]);
 				this._view = mv;
@@ -88,12 +93,97 @@ var Camera = Object.create(Object, {
 		}
 	},
 	
+	_cam : {
+	    value : null,
+		writable: true,
+	},
+	
+	// update the camera position, this is a very crude way to do this,
+	// there is no consideration for the frame rate, so on faster machines
+	// we will move faster... awesome..
+	update: {
+        value: function() {
+		
+            var direction = [0, 0, 0];
+            var speed = 5;
+  
+            if (this._pressed['W'.charCodeAt(0)])
+                direction[1] += speed;
+
+            if (this._pressed['S'.charCodeAt(0)])
+                direction[1] -= speed;
+
+            if (this._pressed['A'.charCodeAt(0)])
+                direction[0] -= speed;
+
+            if (this._pressed['D'.charCodeAt(0)])
+                direction[0] += speed;
+
+			mat4.identity(this._cam);
+			mat4.rotateX(this._cam, this.angles[0]);
+			mat4.rotateZ(this._cam, this.angles[2]);
+			mat4.rotateY(this._cam, this.angles[1]);
+			mat4.inverse(this._cam);
+
+			// multiple the camera matrix by the direction vector
+			mat4.multiplyVec3(this._cam, direction);
+
+			vec3.add(this.position, direction);
+			
+			this._changed = true;
+        }
+    },
+	
 	init: {
 	    value: function(canvas) {
 		    this.angles = vec3.create();
 			this.position = vec3.create();
 			this._view = mat4.create();
-		//	this._camera = mat4.create();
+			this._cam = mat4.create();
+			this._pressed = new Array(128);
+			
+			var moving = false;
+			var lastX = 0;
+			var lastY = 0;
+			var self = this; /* take a copy of this, object so the event listeners use the correct object */
+			
+			canvas.addEventListener('mousedown', function(event) {
+				
+				lastX = event.pageX;
+				lastY = event.pageY;
+				
+			    if (event.which == 1) {
+				    moving = true;
+				}
+			});
+			
+			canvas.addEventListener('mouseup', function(event) {
+                moving = false;
+            });
+			
+			canvas.addEventListener('mousemove', function(event) {
+			    if (moving) {
+				    var deltaX = lastX - event.pageX;
+					var deltaY = lastY - event.pageY;
+					
+					// TODO: handle these wrapping around
+					self.angles[1] -= deltaX * 0.025;
+					self.angles[0] -= deltaY * 0.025;	
+					
+					lastX = event.pageX;
+					lastY = event.pageY;
+					
+					self._changed = true;
+				}
+			});
+			
+			window.onkeydown = function(event) {
+                self._pressed[event.keyCode] = true;
+            }
+
+            window.onkeyup = function(event) {
+                self._pressed[event.keyCode] = false;
+            }
 			
 		    return this;
 		}
